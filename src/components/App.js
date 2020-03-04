b6bbf2b9b493eac00317e6c20564cea246b61041
Mine <mine.kandil@gmail.com>
2020-03-05 00:03:50 +0300

    .

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React, { Component, Fragment } from 'react'
 import { connect } from 'react-redux'
-import { BrowserRouter as Router, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Route,Switch } from 'react-router-dom'
 import LoadingBar from 'react-redux-loading'
 import { handleInitialData } from '../actions/shared'
 import Home from '../components/Home'
@@ -10,6 +10,7 @@ import CreateQuestion from '../components/CreateQuestion'
 import Vote from '../components/Vote'
 import Navbar from './Navbar'
 import PrivateRoute from './PrivateRoute'
+import Page404 from '../components/Page404'
 
 class App extends Component {
   componentWillMount() {
@@ -24,11 +25,14 @@ class App extends Component {
             <Navbar />
             {this.props.loading === true ? null : (
               <div className="container">
+                <Switch>
                 <Route path="/" exact component={Login} />
                 <PrivateRoute path="/home" component={Home} />
                 <PrivateRoute path="/question/:question_id" component={Vote}/>
                 <PrivateRoute path="/add" component={CreateQuestion} />
                 <PrivateRoute path="/leaderboard" component={Leaderboard} />
+                <Route component={Page404} />
+                </Switch>
               </div>
             )}
           </div>
